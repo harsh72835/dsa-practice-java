9a7aa7650a6b186cf0542f6805cec16c55fc6684
Harsh Champakbhai Patel <harsh.patel@protium.co.in>
2022-09-08 00:14:32 +0530

    pattern que-5,6

--- Main course/Patterns.java
+++ Main course/Patterns.java
@@ -4,7 +4,7 @@ public class Patterns {
     //while doing pattern question always start the loop from 1
     public static void main(String[] args) {
         System.out.println("Hello world");
-        pattern3(5);
+        pattern6(5);
     }
 
     static void pattern1(int n){
@@ -42,4 +42,29 @@ public class Patterns {
         }
     }
 
+    static void pattern5(int n){
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern6(int n){
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern7(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                
+            }
+            
+        }
+    }
 }
