502d4f9c9405a572d78b77919d3e1fce9179988a
Harsh <harshpatel8163@gmail.com>
2023-08-27 20:43:47 +0530

    fist 5 star pattern from A2Z course

--- Patterns/StarPattern.java
+++ Patterns/StarPattern.java
@@ -1,17 +1,72 @@
 package Patterns;
 
-class StartPattern {
+class StarPattern {
+    // while doing pattern question always start the loop from 1
     public static void main(String[] args) {
-        System.out.println("helo world");
-        System.out.println(returnN(35));
+        System.out.println("Hello world");
+        pattern6(5);
+    }
 
-    };
-
-    public static int returnN(int n) {
-        int sum = 0;
+    static void pattern1(int n) {
         for (int i = 0; i < n; i++) {
-            sum += i;
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
-        return sum + n;
+    }
+
+    static void pattern2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int n) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern6(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern7(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+            }
+
+        }
     }
 }
