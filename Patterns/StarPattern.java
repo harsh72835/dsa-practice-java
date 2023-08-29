0c82fb64feb922719d260f9fdab4a6078652f660
Harsh <harshpatel8163@gmail.com>
2023-08-29 21:53:51 +0530

    new patterns added

--- Patterns/StarPattern.java
+++ Patterns/StarPattern.java
@@ -1,57 +1,65 @@
 package Patterns;
 
+import java.util.Scanner;
+
 class StarPattern {
     // while doing pattern question always start the loop from 1
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        pattern6(5);
+        // System.out.println("Hello world");
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            // change the method here and run it
+            pattern8(n);
+        }
     }
 
-    static void pattern1(int n) {
+    // pattern8
+    static void pattern8(int n) {
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-    }
-
-    static void pattern2(int n) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-    }
-
-    static void pattern3(int n) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j);
-            }
-            System.out.println();
-        }
-    }
-
-    static void pattern4(int n) {
-        for (int i = 0; i <= n; i++) {
+            // space
             for (int j = 0; j < i; j++) {
-                System.out.print(i);
+                System.out.print(" ");
             }
-            System.out.println();
+
+            // star
+            for (int j = 0; j < (2 * n) - ((2 * i) + 1); j++) {
+                System.out.print("*");
+            }
+
+            // space
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            System.out.println("");
+
         }
     }
 
-    static void pattern5(int n) {
-        for (int i = n; i >= 1; i--) {
-            for (int j = i; j >= 1; j--) {
-                System.out.print("* ");
+    // pattern7
+    static void pattern7(int n) {
+        for (int i = 0; i < n; i++) {
+            // space
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
             }
-            System.out.println();
+
+            // star
+            for (int j = 0; j < (2 * i) + 1; j++) {
+                System.out.print("*");
+            }
+
+            // space
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            System.out.println("");
+
         }
     }
 
+    // pattern 6
     static void pattern6(int n) {
         for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
@@ -61,12 +69,54 @@ class StarPattern {
         }
     }
 
-    static void pattern7(int n) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-
+    // patten5
+    static void pattern5(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("* ");
             }
-
+            System.out.println();
         }
     }
+
+    // pattern4
+    static void pattern4(int n) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    // pattern3
+    static void pattern3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    // pattern2
+    static void pattern2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    // pattern1
+    static void pattern1(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
 }
